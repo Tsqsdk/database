130132641843490783ba64fe4b507ad14bc08084
Tsqsdk <137131768+Tsqsdk@users.noreply.github.com>
2024-03-28 14:33:42 +0800

    insert data

--- index.js
+++ index.js
@@ -39,10 +39,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+    //await client----
     await client.connect();
+
+    //insert document to database
+    client.db("benr_2423").collection("users").insertOne({
+      name:"john",
+      email:"john@gmail",
+      password: "0213",
+    }
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
